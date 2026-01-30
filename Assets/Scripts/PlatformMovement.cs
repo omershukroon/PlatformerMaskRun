31277aad9650364c22503dedbada8841fd5f2ec7
Kim Tsadok <kim.tsadok@gmail.com>
2026-01-31 00:55:45 +0200

    duplicated scene to new branch

--- Assets/Scripts/PlatformMovement.cs
+++ Assets/Scripts/PlatformMovement.cs
@@ -2,20 +2,17 @@ using UnityEngine;
 
 public class PlatformMovement : MonoBehaviour
 {
-    public float speed = 2f;
-    [SerializeField] private bool isActiv = true; // הגדרתי כברירת מחדל כ-true כדי שתראה תנועה מיד
+    public float speed = 5f;
+    [SerializeField] private bool isActiv = true; 
 
     void Update()
     {
-        // בדיקה האם הפלטפורמה אמורה לנוע
         if (isActiv)
         {
-            // תנועה מימין לשמאל: הכפלת הכיוון שמאלה במהירות ובזמן שחלף
             transform.position += Vector3.left * speed * Time.deltaTime;
         }
     }
 
-    // פונקציית עזר למקרה שתרצה להפעיל/לכבות את התנועה מסקריפט אחר (כמו ה-Spawner)
     public void SetMovement(bool status)
     {
         isActiv = status;
